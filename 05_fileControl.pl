a18f77949b00df2313635e56220a9bbe8f9419db
lee-y <rudnfskf2@gmail.com>
2020-06-30 00:31:27 +0900

    fileControl createFile deleteFile

---
+++ 05_fileControl.pl
@@ -0,0 +1,124 @@
+#!/usr/bin/perl
+use strict; 
+use warnings;
+use Cwd;
+
+
+###############################
+###########   Main   ##########
+############################### 
+my @originData = (
+    ['tiger1','tiger2','tiger3'],
+    ['tiger4','tiger5','tiger6'],
+    ['tiger7','tiger8','tiger9'],
+    ['tiger10','tiger11','tiger12'],
+    ['tiger13','tiger14','tiger15'],
+);
+
+
+### Create ####
+my $currentDir = getcwd;
+my $targetDir = "$currentDir/test_dir01";
+print createCsvFile('testfile1.csv',$targetDir,@originData) . "\n";
+
+
+###  Read  ####
+
+
+
+### Update ####
+
+
+
+### Delete ####
+
+
+###############################
+########### Function ##########
+###############################
+
+
+sub createCsvFile {
+    my ($fileName,$targetDir,@data) = @_;
+    print "createCsvFile\n";
+
+    if ($fileName eq '' | $targetDir eq '' | !@data) {
+        print "param is null filename [$fileName], targetDir [$targetDir], data [@data]\n";
+        return '';
+    }
+
+    my $absolutePath = "$targetDir/$fileName";
+
+    # -s File or directory exists and has nonzero size
+    if ( -s $absolutePath) {
+        print "File is already exist! [$absolutePath]\n";
+        return $absolutePath;
+    }
+    
+    # -e File or directory exists 
+    if ( ! -e $targetDir ) {
+        unless(mkdir($targetDir, 0755)) {
+            die "Unable to create Dir [$targetDir]\n";
+        }
+        print "has been created Dir [$targetDir]";
+    }
+    
+    # move to targetDir
+    chdir($targetDir) or die "Unable to change dir [$targetDir]\n";
+
+    # > it can Creates, Writes
+    open(OUT , ">$fileName") or die "Can't make File [$fileName] Where [$targetDir] is.\n";
+
+    foreach my $var (@data) {
+        my @target = @$var;
+        print OUT makeOneStrFromList(",",@target) . "\n";
+    }
+
+    close (OUT);
+
+    # -z  File exists and has zero size (directories are never empty)
+    if (-z $absolutePath) {
+        if (deleteFile($absolutePath) == 0){
+            print "file has been deleted because of it is empty [$absolutePath]!\n";
+        } else {
+            print "failure to delete file [$absolutePath] \n";
+        }
+        return '';
+    }
+
+    return $absolutePath;
+}
+
+
+sub deleteFile {
+    my ($targetFile) = @_;
+    print "deleteFile\n";
+
+    if ($targetFile eq '') {
+       print "param is null\n";
+       return 1;
+    }
+
+    if (! -e $targetFile) {
+       print "[$targetFile] is not exist.\n";
+       return 1;
+    }
+
+    unlink($targetFile) or die "Can't delete $targetFile : $!\n";
+    return 0;
+}
+
+
+sub makeOneStrFromList {
+    my ($seperator,@targetList) = @_;
+    print "makeOneStrFromList\n";
+    
+    if (!@targetList) {
+        print "targetList is null\n";
+        return "";
+    }
+
+    #my $resultStr = join ",", @targetList;
+    my $resultStr = join $seperator, @targetList;
+    return $resultStr;
+}
