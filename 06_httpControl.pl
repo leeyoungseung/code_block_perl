767fc3878f5f6c7f33327c92ba295d176d1546a6
lee-y <rudnfskf2@gmail.com>
2020-07-04 01:18:57 +0900

    httpControl requestPost

--- 06_httpControl.pl
+++ 06_httpControl.pl
@@ -10,7 +10,11 @@ my $testGetUrl = "http://127.0.0.1/index.html";
 requestGet($testGetUrl);
 
 my $testPostUrl = "http://127.0.0.1/postTest.php";
-requestPost($testPostUrl, );
+my %form;
+$form{'id'} = "buildtiger";
+$form{'nickname'} = "tiger11";
+
+requestPost($testPostUrl, %form);
 
 
 
@@ -18,6 +22,27 @@ requestPost($testPostUrl, );
 ###############################
 ########### Function ##########
 ###############################
+sub requestPost {
+    my ($url,%form) = @_;
+    print "requestPost\n";
+
+    if ($url eq '' || $url !~ /http[s]?/ ){
+        print "param is null or fail matching pattern \n";
+    }
+
+    my $ua = LWP::UserAgent -> new;
+    my $response = $ua -> post($url, \%form);
+
+    if ($response -> is_success) {
+        print $response -> decoded_content;
+        print "\n";
+    } else {
+        print $response -> status_line;
+        print "\n";
+    }
+  
+}
+
 
 sub requestGet {
     my ($url) = @_;
